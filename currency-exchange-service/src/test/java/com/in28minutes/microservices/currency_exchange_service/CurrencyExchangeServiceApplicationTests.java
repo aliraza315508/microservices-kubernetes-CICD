252d97acec64281d3f3caa00cf9a2bfc9adfda99
Ali Raza <aliraza315508@gmail.com>
2026-04-10 15:52:09 -0400

    Fix CI and add Test containers for currency-exchnage service

--- currency-exchange-service/src/test/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
+++ currency-exchange-service/src/test/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -2,9 +2,33 @@ package com.in28minutes.microservices.currency_exchange_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class CurrencyExchangeServiceApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres
+			= new PostgreSQLContainer<>
+			("postgres:16-alpine")
+			.withDatabaseName("currencydb")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url" ,  postgres::getJdbcUrl);
+		registry.add("spring.datasource.username" ,  postgres::getUsername);
+		registry.add("spring.datasource.password" ,  postgres::getPassword);
+	}
+
 
 	@Test
 	void contextLoads() {
