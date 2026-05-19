df5062224b1c33dc00b099bd807e857557472431
Ali Raza <aliraza315508@gmail.com>
2026-05-18 23:37:43 -0400

    Allow GitHub Actions access to EKS API

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -19,7 +19,7 @@ variable "kubernetes_version" {
 variable "allowed_cidr" {
   description = "CIDR allowed to access the EKS public API endpoint"
   type        = string
-  default     = "76.124.66.34/32"
+  default     = "0.0.0.0/0"
 }
 
 variable "node_instance_type" {
